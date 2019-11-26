5f04253ddfe2adeff61897fa2aa6934a427bca3f
Mohamed Ouasti <ouastim@gmail.com>
2019-11-26 14:58:17 +0100

    routes dones

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sites, only: [:new, :create, :destroy, :edit, :index]
+  resources :materials, only: [:new, :create, :index, :destroy, :show] do
+    resources :bids
+  end
 end
