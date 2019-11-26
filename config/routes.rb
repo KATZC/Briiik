b1e0ea3a19fd4ac98fbc481b18046b87e707100f
Celine Katz <katz.celine@gmail.com>
2019-11-26 18:07:02 +0100

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'materials/index'
+  get 'materials/show'
+  get 'materials/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
