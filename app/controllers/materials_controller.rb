2dfc2b0da95bfe7bc52dba796069ae7c9615a18a
Julien Nunez <julien.nunez@hotmail.fr>
2019-12-02 17:09:25 +0100

    corrected

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -9,6 +9,7 @@ class MaterialsController < ApplicationController
     @my_posts_pickup = current_user.materials.where(status: 'Vendu')
     @my_posts_done = current_user.materials.where(status: 'Cloturé')
 
+    # A voir avec TA
     # if material.bids.order.any?
     #  @historical_price = current_user.materials.bids.order('price ASC').last&.price
     # else
