5a1a3eafd73e2575d15f7eed5c33dcd5a2eb96b4
Julien Nunez <julien.nunez@hotmail.fr>
2019-12-05 12:24:21 +0100

    cards, order, seed

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,13 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {
+    altInput: true,
+    altFormat: 'd-m-Y',
+    locale: {
+      months: {
+        shorthand: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        longhand: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
+      },
+    },
+  });
