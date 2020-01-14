0f798c9e028b30cbfce649ae211a07c9e263fa65
Celine Katz <katz.celine@gmail.com>
2020-01-14 16:33:52 +0100

    modif sticky footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @nofooter = true
   end
 
-  def remove_footer
-    @homepage = true
-  end
+
 end
